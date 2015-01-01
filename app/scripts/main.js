9bdfda049edec6154eae4364bb0e5fca4f5c0f44
Patrick Love <plove13x@gmail.com>
2015-01-01 18:10:42 -0500

    implements fun facts slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,4 +25,33 @@ $(window).load(function(){
 
     var newSlider = new Slider(imageArray, '.slider-images', 4000, 0);
     var newSlider2 = new Slider(imageArray2, '.slider-images2', 4000, 0);
+
+
+    var funFacts = [
+    '<p>Have you ever gotten bored with plain old Lemon-Lime or Fruit Punch and opted for a Gatorade X-Factor instead? You can thank NHRT (the National Hybrid Research Team) for that. The first website I ever made established the online presence for our admittedly juvenile social club and took the student body by storm in high school. I\'ve been hooked by the power of the web ever since.</p><p>No, we didn\'t reap the financial rewards of such a stroke of mixology brilliance - the idealists we were in high school couldn\'t have been less concerned with intellectual property rights and patents - but we got some serious traffic for a while and then all of a sudden Gatorade started producing their own "hybrids". Unfortunately, the site (www.areyoumixing.com) is no longer accessible due to an overhaul of the Tripod free hosting program, but long live the memories and keep on mixing!</p>',
+    '<p>Speaking of high school, I (thanks to some timely self-deprecating humor) was elected as both junior and senior class president of my high school class and gave our graduation speech. I also served as Managing Editor of our high school newspaper that won 1st place at the <em>Dallas Morning News\’</em> Journalism Day competition. Dork!</p>',
+    '<p>I love basketball and after college I once tried out for an American Basketball Association team. Ultimately I wasn\'t offered a contract, but on the plus side, I was invited back for another tryout. On the not-so-plus side, one large man who was already under contract chose to refer to me exclusively as "Little Man". This was the first and only time in my life this has happened.</p>',
+    '<p>Some might say in my younger years I could be a bit of a problem child, but I wasn\'t the <em>Problem Child</em>. However, since some of the movie\'s birthday party scene was filmed at our house, a keen eye will notice young me in a lion costume. Any Oscar noms went to my Mom though for her role as "woman gets pinned on the ass". Check it out on YouTube: <a target="_blank" href="https://www.youtube.com/watch?v=KhAg5po8InY"><em>Problem Child</em> party scene clip</a></p>',
+    '<p>My junior high (5th-8th grade) holds an annual schoolwide spelling bee, and somehow I won the whole thing twice (6th and 8th grade).</p>'
+    ];
+
+    var counterFF = 0;
+
+    $('.sliderFFContainer button').on('click', function(){
+        if ($(this).hasClass('forward')) {
+            counterFF = (counterFF + 1);
+        } else {
+            counterFF = (counterFF - 1);
+        }
+
+        var fFP = $('.fFact');
+        fFP.fadeOut(600, function() {
+            if (counterFF >= 5) { counterFF = 0 }
+            if (counterFF < 0) { counterFF = 4 }
+            fFP.html(funFacts[counterFF]);
+            fFP.fadeIn(3000);
+        });
+
+    });
+
 });
